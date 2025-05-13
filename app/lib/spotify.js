ec048ed5147d1c715c56e3a7586ea9e3473752b2
Bobbasrinivasareddy834 <srinivasareddybobba208@gmail.com>
2025-05-13 17:37:09 +0530

    first commit

--- app/lib/spotify.js
+++ app/lib/spotify.js
@@ -8,7 +8,6 @@ const REDIRECT_URI = 'http://127.0.0.1:3000/callback';
 // const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 // const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 // const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
-// console.log(CLIENT_ID,"TEST")
 export async function getValidAccessToken() {
   const accessToken = localStorage.getItem('spotify_access_token');
   const refreshToken = localStorage.getItem('spotify_refresh_token');
